76932fcb6abe189be5161cc294071ce4c26d427d
kasia.kordys <kasia.kordys@gmail.com>
2023-09-21 16:12:57 +0300

    add usesearchparams to set url params

--- src/BookLayout.jsx
+++ src/BookLayout.jsx
@@ -1,14 +1,24 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 const BookLayout = () => {
+  const [searchParams, setSearchParams] = useSearchParams({ n: 3 });
+  console.log(searchParams.get("n"));
+  const number = searchParams.get("n");
   return (
     <>
       <Link to="/books/1">Book 1</Link>
       <br />
       <Link to="/books/2">Book 2</Link>
       <br />
+      <Link to={`/books/${number}`}>Book {number}</Link>
+      <br />
       <Link to="/books/new">New Book</Link>
       <Outlet context={{ book: "review" }} />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setSearchParams({ n: e.target.value })}
+      />
     </>
   );
 };
